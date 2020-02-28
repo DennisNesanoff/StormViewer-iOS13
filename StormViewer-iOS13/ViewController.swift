eb93eb9816a506a73017347a90cd4f62f9cb957d
Dennis <nndenninn@gmail.com>
2020-02-28 19:03:37 +0300

    Initial Commit

---
+++ StormViewer-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer-iOS13
+//
+//  Created by Dennis Nesanoff on 28.02.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
